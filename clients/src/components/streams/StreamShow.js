532b2d8a06f77f9169907b187f6bd62740ce7789
Brijesh <brijeshb.it18@nsut.ac.in>
2020-11-24 20:11:12 +0530

    Added Streaming Flv
    Streaming Flv
    (app completed)

--- clients/src/components/streams/StreamShow.js
+++ clients/src/components/streams/StreamShow.js
@@ -1,39 +1,64 @@
 import React from "react";
 import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
+import flv from 'flv.js';
+class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
 
-class StreamShow extends React.Component{
-componentDidMount()
-{
-    this.props.fetchStream(this.props.match.params.id);
+    this.videoRef = React.createRef();
+  }
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+  componentWillUnmount = () => {
+    this.player.destroy();
+  };
+
+  buildPlayer() {
+        const { id } = this.props.match.params;
+   
+        if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading your Stream...</div>;
+    }
+
+    const { title, description } = this.props.stream;
+    return (
+      <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
+        <h1>{title}</h1>
+
+        <h5>{description}</h5>
+      </div>
+    )
+  }
 }
 
-render()
+
+const mapStateToProps =(state ,ownProps)=>
 {
-
- if(!this.props.stream)
-{
-    return <div>Loading....</div>;
+    return{stream : {...state.streams[ownProps.match.params.id]}}
 }
-
-const {title ,description} =this.props.stream;
-
-return (
-  <div>
-    <h1>{title}</h1>
-    <h5>{description}</h5>
-  </div>
-);
-}
-
-}
-
-const mapStateToProps = (state,ownProps)=>
-{
-    return {stream: state.stream[ownProps.match.params.id]}
-};
-
-export default connect(
-    null , 
-    {fetchStream}
-    )(StreamShow);
+    export default connect(
+        mapStateToProps,
+        {fetchStream}
+    ) (StreamShow);
